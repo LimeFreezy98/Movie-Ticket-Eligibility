7a47a51165bbcf87274fda5e377ecb1e3ecc4b68
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-09-15 08:56:40 -0600

    adding javascript function to able to tell able get discount or not

--- movie-ticket-eligibility.js
+++ movie-ticket-eligibility.js
@@ -7,3 +7,15 @@ let isStudent = true;
 
 // TODO: Write an if/else statement using booleans
 // to decide which message to log.
+
+// Business Rules:
+// Eligible if under 18 years old OR a student
+if (age < 18 || isStudent === true) {
+    message =("Discount ticket granted ✅");
+} else {
+    message =("Regular ticket only ❌");
+}
+
+// display result in html
+document.getElementById("result").innerText = message;
+
